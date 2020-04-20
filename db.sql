0a39e8144d361d307569f1a89dcf39f0f26b72f4
Phisarn Budwong <o-hite@hotmail.com>
2020-04-21 00:09:11 +0700

    update

--- db.sql
+++ db.sql
@@ -8,12 +8,12 @@ CREATE TABLE peers
   port smallint(1) unsigned NOT NULL DEFAULT '0',
   crypt bit(1) NOT NULL DEFAULT b'0',
   residual bigint(1) unsigned NOT NULL DEFAULT '0',
-  added timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  added timestamp NOT NULL DEFAULT 0,
   updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (pid),
   UNIQUE KEY uid (uid,tid),
   KEY tid (tid)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE torrents 
 (
@@ -22,13 +22,13 @@ CREATE TABLE torrents
   name varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   downloaded int(1) unsigned NOT NULL DEFAULT '0',
   banned bit(1) NOT NULL DEFAULT b'0',
-  added timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  added timestamp NOT NULL DEFAULT 0,
   updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (tid),
   UNIQUE KEY infohash (infohash),
   KEY banned (banned),
   FULLTEXT KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
 ALTER TABLE peers ADD CONSTRAINT peers_ibfk_1 FOREIGN KEY (tid) REFERENCES torrents (tid) ON DELETE CASCADE ON UPDATE NO ACTION;
--- upload/backend/config.php
+++ upload/backend/config.php
@@ -9,11 +9,11 @@
    $config = ( object ) Array 
    (
       /* Db Settings */
-      'dbDsn'  => 'mysql:host=localhost;dbname=open;charset=utf8',
+      'dbDsn'  => 'mysql:host=localhost;dbname=admin_;charset=utf8',
        
-      'dbUser' => 'root',
+      'dbUser' => 'dbsql',
        
-      'dbPass' => '',
+      'dbPass' => 'Jb4&0ti8',
       
       /* Tracker Settings */
       'minInterval' => 300,
